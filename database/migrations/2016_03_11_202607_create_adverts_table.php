feadbcb72408a965a331d1d843e52fe4e9de5be6
Yehia Elassouli <yehiaass1997@gmail.com>
2019-12-31 00:21:17 +0200

    [X]publish migration for package advert

---
+++ database/migrations/2016_03_11_202607_create_adverts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdvertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('alt');
+            $table->string('url');
+            $table->string('image_url')->nullable();
+            $table->string('image_path')->nullable();
+            $table->integer('views')->default(0);
+            $table->integer('clicks')->default(0);
+            $table->boolean('active');
+            $table->integer('advert_category_id')->unsigned();
+            $table->timestamp('viewed_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('advert_category_id')
+                ->references('id')
+                ->on('advert_categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('adverts');
+    }
+}
